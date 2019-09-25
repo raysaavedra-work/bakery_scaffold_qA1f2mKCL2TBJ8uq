6dfe8aa2c36589abfb7840877604b6312cdca7f8
raysaavedra-work <rayyacosaavedra@gmail.com>
2019-09-25 07:57:08 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
 class TestAcceptanceStripe(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestAcceptanceStripe, self).__init__(*args, **kwargs)
@@ -17,7 +18,7 @@ class TestAcceptanceStripe(unittest.TestCase):
 
     def test_acceptance_stripe_public_key_has_been_set(self):
         """Check if Stripe key was defined."""
-        pattern = re.compile(r"Stripe\('pk_test_\w{24}'\);", re.I | re.M)
+        pattern = re.compile(r"Stripe\('pk_test_\w+'\);", re.I | re.M)
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define the Stripe key.")
 
@@ -32,7 +33,8 @@ class TestAcceptanceStripe(unittest.TestCase):
     def test_acceptance_checkout_button_was_instantiated(self):
         """Check if checkout button was captured."""
         pattern = re.compile(
-            r"document.getElementById\('checkout-button-sku_\w{14}'\);", re.I | re.M)
+            r"document.getElementById\('checkout-button-sku_\w{14}'\);", re.I | re.M
+        )
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'),
                         msg="You didn't add a checkout button.")
@@ -66,6 +68,96 @@ class TestAcceptanceStripe(unittest.TestCase):
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define a cancel URL.")
 
- 
-if __name__ == '__main__':
+
+class AssessmentTestCases(unittest.TestCase):
+    def setUp(self):
+
+        with open("order.html", "r") as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+
+        WINDOW_SIZE = "1920,1080"
+
+        options = selenium.webdriver.ChromeOptions()
+        options.headless = True
+        options.binary_location = os.getenv('CHROME_BIN')
+        options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        options.add_argument("--disable-gpu")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        self.driver = webdriver.Chrome(os.getenv('CHROMEDRIVER_PATH'), options=options)
+
+    def _get_button_id(self):
+        pattern = re.compile(
+            r"\('checkout-button-sku_\w{14}'\);", re.I | re.M
+        )
+        res = re.search(pattern, self.dom_str)
+        return res.group().split("'")[1]
+
+    def _get_url(self):
+        pattern = re.compile(
+            r"cancelUrl: \'(http|https)://(.*)/order.html\'", re.I | re.M
+        )
+        res = re.search(pattern, self.dom_str)
+        return ":".join(res.group().split(": ")[1:]).strip("'")
+
+    def test__successful_payment_on_the_checkout_page_redirects_to_order_html__clientcheckout__2(
+        self
+    ):
+        self.driver.get(self._get_url())
+        wait = WebDriverWait(self.driver, 20)
+
+        elem = wait.until(EC.presence_of_element_located((By.ID, self._get_button_id())))
+        elem.click()
+
+        email_elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
+
+        cardnum_elem = self.driver.find_element_by_id("cardNumber")
+        cardexp_elem = self.driver.find_element_by_id("cardExpiry")
+        cardcvc_elem = self.driver.find_element_by_id("cardCvc")
+        cardname_elem = self.driver.find_element_by_id("billingName")
+
+        try:
+            zip_elem = self.driver.find_element_by_id('billingPostalCode')
+        except:
+            zip_elem = None
+
+        email_elem.send_keys("assessment@test.com.br")
+        cardnum_elem.send_keys("555555555555")
+        cardnum_elem.click()
+        cardnum_elem.send_keys("4444")
+        cardexp_elem.send_keys("0439")
+        cardcvc_elem.send_keys("424")
+        cardname_elem.send_keys("Selenium Test WebDriver")
+
+        if zip_elem:
+            zip_elem.send_keys('12312')
+
+        confirm_elem = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "SubmitButton--complete")))
+        confirm_elem.click()
+
+        session_id_elem = wait.until(
+            EC.presence_of_element_located((By.ID, "sessionId"))
+        )
+
+        self.assertIn("order_success.html", self.driver.current_url)
+        self.assertTrue(session_id_elem.text)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+    
+
+class TestSubmission(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSubmission, self).__init__(*args, **kwargs)
+        with open('order.html', 'r') as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+    
+
+
+if __name__ == "__main__":
     unittest.main()
